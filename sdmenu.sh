99322f7e480aad7c3f91a3de1d252720426ccbf3
Страхиња Радић <contact@strahinja.org>
2020-10-14 16:35:30 +0200

    Smarter shutdown/reboot

--- sdmenu.sh
+++ sdmenu.sh
@@ -3,10 +3,15 @@
 echo=/usr/bin/echo
 command=$($echo -ne "Do nothing\nShutdown\nRestart" | dmenu -p "  Shutdown?")
 
+shutdown=$([ -f /sbin/shutdown ] && $echo "/sbin/shutdown -h now" \
+    || $echo "loginctl poweroff")
+restart=$([ -f /sbin/shutdown ] && $echo "/sbin/shutdown -r now" \
+    || $echo "loginctl reboot")
+
 case $command in
-	Shutdown) loginctl poweroff
+	Shutdown) $shutdown
 		  ;;
-	Restart)  loginctl reboot
+	Restart)  $restart
 		  ;;
 	*)	  ;;
 esac
