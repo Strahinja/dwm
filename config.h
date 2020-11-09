3a6481fec237c321281861630314a854468dfcfa
Страхиња Радић <contact@strahinja.org>
2020-11-09 20:04:18 +0100

    Changed font to Terminus + Noto Color Emoji

--- config.h
+++ config.h
@@ -9,9 +9,11 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Cozette Vector:size=9" };
+static const char *fonts[]          = { "xos4 Terminus:size=10" };
+//static const char *fonts[]          = { "Cozette Vector:size=9" };
 //static const char *fonts[]          = { "PxPlus_IBM_VGA8 Nerd Font:size=12" };
-static const char dmenufont[]       = "Cozette Vector:size=9";
+static const char dmenufont[]       = "xos4 Terminus:size=10";
+//static const char dmenufont[]       = "Cozette Vector:size=9";
 //static const char dmenufont[]       = "PxPlus_IBM_VGA8 Nerd Font:size=12";
 static const char col_gray1[]       = "#121212";
 static const char col_gray2[]       = "#444444";
