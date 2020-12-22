14e1d7ee77d60289e6d8206121693e9492e28342
Страхиња Радић <contact@strahinja.org>
2020-12-22 22:18:16 +0100

    Added tmux
    Signed-off-by: Страхиња Радић <contact@strahinja.org>

--- config.h
+++ config.h
@@ -38,10 +38,10 @@ static const char *colors[][3]      = {
 };
 
 static const char *const autostart[] = {
-    "st", NULL,
+    "st", "-e", "tmux", "-u", NULL,
     "setxkbmap", "-layout", "us,rs", "-option", "grp:alt_shift_toggle", NULL,
     "picom", "--experimental-backends", NULL,
-    "sh", "-c", "~/.fehbg", NULL,
+    "bash", "-c", "~/.fehbg", NULL,
     "slstatus", NULL,
     "twmnd", NULL,
     NULL /* terminate */
@@ -93,7 +93,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_white, "-sb", col_green1, "-sf", col_white, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "tmux", "-u", NULL };
 static const char *sdmenucmd[] = { "sdmenu", NULL };
 static const char *pickcharcmd[] = { "pickchar", NULL };
 static const char *slockcmd[] = { "slock", NULL };
